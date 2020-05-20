1730c3312dd988bcee71de1471b70cd87d93b258
Sakshi Lajurkar <lajurkar.sakshi@gmail.com>
2020-05-20 20:43:07 +0530

    User model and request validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,19 @@
-const express = require('express');
+const express = require('express')
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => res.send('User route'));
+router.post('/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Enter valid email id').isEmail(),
+    check('password', 'Enter password more than 6 characters').isLength({min:6})
+  ],
+(req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(400).json({ errors: errors.array() });
+  }
+  res.send('User Route')
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const app = express();
 
 connectDB();
+app.set('view engine', 'ejs');
+app.use(express.json({ extended: false}));
 
 app.get('/', (req, res) => res.send('API Running'));
 
 app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/profile', require('./routes/api/profile'));
+// app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/posts', require('./routes/api/posts'));
+// app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 8000;
 
