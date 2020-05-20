692730148b68e487bd2d06f142b3f1f26c18ea30
Sakshi Lajurkar <lajurkar.sakshi@gmail.com>
2020-05-20 14:19:09 +0530

    db connected and route add

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = congig.get('mongoURI');
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try{
-    await.mongoose.connect(db);
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
     console.log('MongoDB Connected');
   }
   catch(err){
