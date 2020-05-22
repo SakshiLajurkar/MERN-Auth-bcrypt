be8d4afa8efb4bb100fbe1c8ae2efc8545c0e588
Sakshi Lajurkar <lajurkar.sakshi@gmail.com>
2020-05-22 13:15:14 +0530

    user registration done

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 router.post('/',
   [
@@ -8,12 +11,36 @@ router.post('/',
     check('email', 'Enter valid email id').isEmail(),
     check('password', 'Enter password more than 6 characters').isLength({min:6})
   ],
-(req, res) => {
+async (req, res) => {
   const errors = validationResult(req);
-  if(!errors.isEmpty()){
+  if(!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  res.send('User Route')
+
+const {name, email, password} = req.body;
+
+try{
+  let user = await User.findOne({email});
+
+  if(user){
+    return res.status(400).json({errors: [{message: 'User exists'}]})
+  }
+
+  user = new User({
+    name,
+    email,
+    password
+  });
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(password, salt);
+  await user.save();
+
+  res.send('User Registered');
+} catch(err){
+  console.error(err.message);
+  res.status(500).send('Server error');
+}
 });
 
 module.exports = router;
