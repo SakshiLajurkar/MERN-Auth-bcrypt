bf5042ec5a08621bf11a6081f5e42ee06af3a541
Sakshi Lajurkar <lajurkar.sakshi@gmail.com>
2020-05-26 15:03:54 +0530

    jsonwebtoken ipmlementation

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json({ extended: false}));
 app.get('/', (req, res) => res.send('API Running'));
 
 app.use('/api/users', require('./routes/api/users'));
-// app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/auth', require('./routes/api/auth'));
 // app.use('/api/posts', require('./routes/api/posts'));
 // app.use('/api/profile', require('./routes/api/profile'));
 
